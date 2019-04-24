4b159355f52d93437899dd16de52f4cdb9a972c3
dianaxime <35496688+dianaxime@users.noreply.github.com>
2019-04-23 20:02:29 -0600

    Add files via upload

--- fun.s
+++ fun.s
@@ -1,90 +1,90 @@
 .global horizontal, vertical, conversion
 
 horizontal:
-push {lr}
-@agregar horizontalmente
-@ ro contiene la direccion de memoria del vector
-@ r1 contiene un int con la casilla que el usuario selecciono
-@sumo las direcciones de memoria
-add r0, r0, r1
-mov r2,#'+'
-@se itera 3 veces por el espacio que ocupa el barco
-mov r3,#3
-repetir: strb r2, [r0], #1
-subs r3, r3, #1
-bne repetir
-pop {pc}
+	push {lr}
+	@agregar horizontalmente
+	@ ro contiene la direccion de memoria del vector
+	@ r1 contiene un int con la casilla que el usuario selecciono
+	@sumo las direcciones de memoria
+	add r0, r0, r1
+	mov r2,#'+'
+	@se itera 3 veces por el espacio que ocupa el barco
+	mov r3,#3
+	repetir: strb r2, [r0], #1
+	subs r3, r3, #1
+	bne repetir
+	pop {pc}
 
 
 vertical:
-push {lr}
-@agregar verticalmente
-@ ro contiene la direccion de memoria del vector
-@ r1 contiene un int con la casilla que el usuario selecciono
-add r0, r0, r1
-mov r2,#'+'
-mov r3,#3
-repet: strb r2, [r0], #5
-subs r3, r3, #1
-bne repet
-pop {pc}
+	push {lr}
+	@agregar verticalmente
+	@ ro contiene la direccion de memoria del vector
+	@ r1 contiene un int con la casilla que el usuario selecciono
+	add r0, r0, r1
+	mov r2,#'+'
+	mov r3,#3
+	repet: strb r2, [r0], #5
+	subs r3, r3, #1
+	bne repet
+	pop {pc}
 
 conversion:
-push {lr}
-@r0 contiene la direccion de la letra
-@Compara con las letras y segun caso lo convierte a un numero
-ldrb r0, [r0]
-cmp r0, #'A'
-moveq r2, #0
-cmp r0, #'B'
-moveq r2, #1
-cmp r0, #'C'
-moveq r2, #2
-cmp r0, #'D'
-moveq r2, #3
-cmp r0, #'E'
-moveq r2, #4
-cmp r0, #'F'
-moveq r2, #5
-cmp r0, #'G'
-moveq r2, #6
-cmp r0, #'H'
-moveq r2, #7
-cmp r0, #'I'
-moveq r2, #8
-cmp r0, #'J'
-moveq r2, #9
-cmp r0, #'K'
-moveq r2, #10
-cmp r0, #'L'
-moveq r2, #11
-cmp r0, #'M'
-moveq r2, #12
-cmp r0, #'N'
-moveq r2, #13
-cmp r0, #'O'
-moveq r2, #14
-cmp r0, #'P'
-moveq r2, #15
-cmp r0, #'Q'
-moveq r2, #16
-cmp r0, #'R'
-moveq r2, #17
-cmp r0, #'S'
-moveq r2, #18
-cmp r0, #'T'
-moveq r2, #19
-cmp r0, #'U'
-moveq r2, #20
-cmp r0, #'V'
-moveq r2, #21
-cmp r0, #'W'
-moveq r2, #22
-cmp r0, #'X'
-moveq r2, #23
-cmp r0, #'Y'
-moveq r2, #24
-mov r0, r2
-pop {pc}
+	push {lr}
+	@r0 contiene la direccion de la letra
+	@Compara con las letras y segun caso lo convierte a un numero
+	ldrb r0, [r0]
+	cmp r0, #'A'
+	moveq r2, #0
+	cmp r0, #'B'
+	moveq r2, #1
+	cmp r0, #'C'
+	moveq r2, #2
+	cmp r0, #'D'
+	moveq r2, #3
+	cmp r0, #'E'
+	moveq r2, #4
+	cmp r0, #'F'
+	moveq r2, #5
+	cmp r0, #'G'
+	moveq r2, #6
+	cmp r0, #'H'
+	moveq r2, #7
+	cmp r0, #'I'
+	moveq r2, #8
+	cmp r0, #'J'
+	moveq r2, #9
+	cmp r0, #'K'
+	moveq r2, #10
+	cmp r0, #'L'
+	moveq r2, #11
+	cmp r0, #'M'
+	moveq r2, #12
+	cmp r0, #'N'
+	moveq r2, #13
+	cmp r0, #'O'
+	moveq r2, #14
+	cmp r0, #'P'
+	moveq r2, #15
+	cmp r0, #'Q'
+	moveq r2, #16
+	cmp r0, #'R'
+	moveq r2, #17
+	cmp r0, #'S'
+	moveq r2, #18
+	cmp r0, #'T'
+	moveq r2, #19
+	cmp r0, #'U'
+	moveq r2, #20
+	cmp r0, #'V'
+	moveq r2, #21
+	cmp r0, #'W'
+	moveq r2, #22
+	cmp r0, #'X'
+	moveq r2, #23
+	cmp r0, #'Y'
+	moveq r2, #24
+	mov r0, r2
+	pop {pc}
 
 
--- main.s
+++ main.s
@@ -43,6 +43,38 @@ navesjuno: ldr r0, =mensaje_ingreso
 ldr r0, =mensaje_bj2
 bl puts
 
+mov r11, #0
+navesjdos: ldr r0, =mensaje_ingreso
+	bl puts
+	@Ingresa la letra correspondiente a la casilla y la guarda en memoria
+	ldr r0, =formato
+	ldr r1, =letra
+	bl scanf
+	@Ingresa la orientacion del barco con una letra y lo guarda en memoria
+	ldr r0, =mensaje_orientacion
+	bl puts
+	ldr r0, =formato
+	ldr r1, =orientacion
+	bl scanf
+	@Convierte la letra de la casilla a un numero
+	ldr r0, =letra
+	bl conversion
+	@Carga el valor de la casilla y la direccion de memoria del vector
+	mov r1, r0
+	ldr r0, =mapa1j2
+	@Compara si es horizontal o vertical y llama a la subrutina segun sea el caso
+	ldr r4, =orientacion
+	ldrb r4, [r4]
+	cmp r4, #'V'
+	bleq vertical
+	cmp r4, #'H'
+	bleq horizontal
+	ldr r0, =mapa1j2
+	bl cuadro
+	add r11, r11, #1
+	cmp r11,#2
+	bne navesjdos
+
 
 
 
@@ -61,7 +93,7 @@ bl puts
  formato: .asciz "   %c"
  letra: .byte 0
  orientacion: .byte 0
- mensaje_bj1: .asciz "Jugador1: escoja sus naves"
-  mensaje_bj2: .asciz "Jugador2: escoja sus naves"
+ mensaje_bj1: .asciz "Jugador 1: escoja sus naves"
+ mensaje_bj2: .asciz "Jugador 2: escoja sus naves"
  mensaje_ingreso: .asciz "Ingrese una letra\n"
  mensaje_orientacion: .asciz "Ingrese V para vertical o H para horizontal"
